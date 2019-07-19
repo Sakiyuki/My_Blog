7ee23be0a661bbefdf8461ffc32ce11346c7a4e8
C-Well <iambryantcaldwell@gmail.com>
2019-07-19 09:26:02 -0400

    My_Blog
    Worked on slug and routes for blog

---
+++ My_Blog/Controllers/BlogPostsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using My_Blog.Models;
+using My_Blog.Utilities;
+
+namespace My_Blog.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: BlogPosts
+        
+        // This is my BlogPosts Indes for the public to see.. Nobody sees Posts that are not marked as published
+        public ActionResult Index()
+        {
+            //var allBlogPosts = db.BlogPosts.Where(b => b.Published).ToList();
+            //retrn View(allBlogPosts);
+            return View(db.BlogPosts.ToList());
+        }
+
+        // GET: BlogPosts/Details/5
+        public ActionResult Details(string Slug)
+        {
+            if (String.IsNullOrWhiteSpace(Slug))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.FirstOrDefault(p => p.Slug == Slug);
+            //var blogPost = db.BlogPosts.FirstOrDefault(b => b.Slug == slug);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BlogPosts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Body,MediaUrl,Published")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                var Slug = StringUtilities.URLFriendly(blogPost.Title);
+                if (String.IsNullOrWhiteSpace(Slug))
+                {
+                    ModelState.AddModelError("Title", "Invalid title");
+                    return View(blogPost);
+                }
+                if(db.BlogPosts.Any(p=> p.Slug == Slug))
+                {
+                    ModelState.AddModelError("Title", "The title must be unique");
+                    return View(blogPost);
+                }
+
+                blogPost.Slug = Slug;
+                blogPost.Created = DateTimeOffset.Now;
+                db.BlogPosts.Add(blogPost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Abstract,Slug,Body,MediaUrl,Published,Created,Updated")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {//This is where we recreate the slug based soley on the incoming Title from the form
+                var newSlug = StringUtilities.URLFriendly(blogPost.Title);
+
+                db.Entry(blogPost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            db.BlogPosts.Remove(blogPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- My_Blog/Models/BlogPost.cs
+++ My_Blog/Models/BlogPost.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace My_Blog.Models
 {
@@ -11,6 +12,7 @@ namespace My_Blog.Models
         public string Title { get; set; }
         public string Abstract { get; set; }
         public string Slug { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
         public string MediaUrl { get; set; } // This URL will record the path of the media or image.
         public bool Published { get; set; } // This will only publish once the blog is completd.
